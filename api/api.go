c340e06b533921564367b9e07385e8ab80d42930
vforfreedom96@gmail.com <vforfreedom96@gmail.com>
2024-04-30 18:00:25 +0800

    chore:remove .mk

---
+++ api/api.go
@@ -0,0 +1,10 @@
+package api
+
+import(
+	_ "github.com/begonia-org/go-sdk/api/app/v1"
+	_ "github.com/begonia-org/go-sdk/api/endpoint/v1"
+	_ "github.com/begonia-org/go-sdk/api/user/v1"
+	_ "github.com/begonia-org/go-sdk/api/file/v1"
+	_ "github.com/begonia-org/go-sdk/api/iam/v1"
+	_ "github.com/begonia-org/go-sdk/api/plugin/v1"
+)
